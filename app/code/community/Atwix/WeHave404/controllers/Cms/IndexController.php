e2becf884f4af8947edf184ed9969672ed4f3a1c
Maxim Uroda <umax@atwix.com>
2015-09-04 11:34:42 +0200

    Added notice of license, minor refactoring.

--- app/code/community/Atwix/WeHave404/controllers/Cms/IndexController.php
+++ app/code/community/Atwix/WeHave404/controllers/Cms/IndexController.php
@@ -1,7 +1,24 @@
 <?php
+/**
+ * Atwix
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ * @category    Atwix Mod
+ * @package     Atwix_WeHave404
+ * @author      Atwix Core Team
+ * @copyright   Copyright (c) 2015 Atwix (http://www.atwix.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
-require_once 'Mage/Cms/controllers/IndexController.php';
-
+require_once Mage::getModuleDir('controllers', 'Mage_Cms') . DS . 'IndexController.php';
 
 class Atwix_WeHave404_Cms_IndexController extends Mage_Cms_IndexController
 {
